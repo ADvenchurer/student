35105646df1952d4a342d19dfb6067e5af47a826
ADvenchurer <adamekwalker@gmail.com>
2024-12-04 18:58:16 +0100

    Add files via upload

---
+++ person.py
@@ -0,0 +1,28 @@
+import pygame
+
+class student:
+    
+    school = "oxford"
+    name = ""
+    age = 0
+    grade = 0
+'''    name = input("What is the student's name?")
+    age = input("How old is the student?")
+    grade = input("What grade is the student im?")'''
+
+
+
+    def change_details():
+        self.name = input("What would you like to change the student's name to?")
+        self.age = input("What would you like to change the student's age to?")
+        self.grade = input("what would you like to change the student's grade to?")
+
+    change_details()
+
+
+
+
+Adam = student()
+
+
+
